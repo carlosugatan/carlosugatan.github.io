6a9ef7ffa47eed480d91718a156ce7055bc0687c
Carlo Sugatan <carlosugatan@gmail.com>
2019-04-19 01:37:16 -0400

    added gallery and contact page

---
+++ si539-final/js/formvalidation.js
@@ -0,0 +1,11 @@
+  $(document).ready(function() {
+    $('select').material_select();
+
+    // for HTML5 "required" attribute
+    $("select[required]").css({
+      display: "inline",
+      height: 0,
+      padding: 0,
+      width: 0
+    });
+  });
